6a63701bc20e187800aa64810683b272cf2ef716
Brian Van Peski <bvp663@gmail.com>
2023-02-02 00:19:12 -0800

    V1.5 (#4)
    * first draft of v1.5
    
    ### Added
    - Added ability to always prompt user to log out of Find My regardless of activation lock status.
    - Script now grabs the iCloud email address of the user who activation locked the device.
    - Added user input for sleep timer.
    - Added timeout w/ user input.
    
    ### Changed
    - Changed default icon to FindMy.icns
    
    * updating defaults
    
    * adding timeout variable and extra osascript activation
    
    * SwiftDialog Support
    
    ## Bug Fixes
    - There were places where two single quotes were used, but likely that double quotes were intended. I corrected this.
    
    ## Enhancements
    - Added `dialogPath` variable for the SwiftDialog binary
    - Added `swiftDialogOptions` array to user configuration section so admins can customize the behavior of swiftdialog
    - I reworked the logic of the User_Dialog function hoping to simplify it
     - Since both Kandji and SwiftDialog use the same syntax for `--icon "$appIcon"`, I set this as an array by itself. If the app icon doesn't exist, the array is just whitespace and doesn't affect the command to call the dialog window
     - I simplified the if/elif section and added SwiftDialog into the mix. The script will try to use Kandji, then SwiftDialog, then osascript.
    
    * Additional Dialog Verification
    
    ## Enhancements
    - Added an additional check to verify both components of swiftDialog are present
    
    * Updating CHANGELOG
    
    * Updating README
    
    * Final cleanup for v1.5
    
    * README corrections
    
    * updating CHANGELOG
    
    ---------
    
    Co-authored-by: BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A tool for helping prevent user-based Activation Lock issues.
 <img src="images/activationunlock_light.png" img align="left" width=30%>
 
-This script checks to see if a machine has Activation Lock enabled, and if so, it will try to determine if the currently logged in user is the one associated with the activation lock, and prompt the user to turn off Find My Mac. If the device is enrolled in an MDM, this will give that MDM solution enough time to prevent future activation locks and gather an activation lock bypass code should the activation lock ever get turned back on.
+This script checks to see if a machine is Activation locked, and if so, it will try to determine if the currently logged in user is the one associated with the activation lock, and prompt the user to turn off Find My Mac. If the device is enrolled in an MDM, this will give that MDM solution enough time to prevent future Activation Lock and gather an Activation Lock bypass code should the Activation Lock ever get turned back on. There is also an option to *always* prompt a user to log out of Find My Mac regardless of Activation Lock status.
 
 This script is designed to assist with *existing* devices that were enrolled into an MDM when a user on the device is already logged into iCloud with Find My Mac enabled at the time of enrollment. To prevent activation lock on NEW enrollments, I **highly suggest** you enroll your devices using Automated Device Enrollment. That is the best way to avoid activation lock from happening in the first place. You can find more thoughts around user-based Activation Lock over on the [blog](https://www.macosadventures.com/2023/01/30/a-guide-to-disabling-preventing-icloud-activation-lock).
 
@@ -11,14 +11,14 @@ This script has been tested (somewhat) on macOS Montery 12.4 and macOS Ventura 1
 ## Customizing the dialog
 While this script was designed with Kandji in mind, it is designed to be plug-and-play for just about any MDM.
 
-I’ve included two options for messaging the end-user leveraging the Kandji CLI or standard osascript, but feel free to add your messaging binary of choice if you prefer using <a href="https://github.com/bartreardon/swiftDialog" title="">SwiftDialog</a>, <a href="https://github.com/julienXX/terminal-notifier" title="">Terminal Notifier</a>, or other solutions.
+I’ve included three options for messaging the end-user leveraging the Kandji CLI, Swift Dialog, or standard osascript, but feel free to add your messaging binary of choice if you prefer using your native MDM messaging system or a different third party messaging tool.
 
 I've included the ability to add an appIcon to the messaging. You can deploy and integrate your own custom icon, or use one that already exists on the machine. Here are a few suggestions:
 `/System/Library/PrivateFrameworks/AOSUI.framework/Versions/A/Resources/AppleID.icns`
 `/System/Library/PrivateFrameworks/AOSUI.framework/Versions/A/Resources/findmy.icns`
 `/System/Library/PrivateFrameworks/AOSUI.framework/Versions/A/Resources/iCloud.icns`
 
-I've set the iCloud icon as the default, since that helps the end-user visually understand the section of System Settings they need to be looking for, but feel free to customize this to suit your needs.
+I've set the FindMy icon as the default, since that helps the end-user visually understand the section of System Settings they need to be looking for, but feel free to customize to suit your needs.
 
 **Pro Tip:** osascript dialogs look pretty boring and dated these days in macOS, but adding a path to an app icon goes a long ways towards making it look less terrible.
 
@@ -36,14 +36,8 @@ I've set the iCloud icon as the default, since that helps the end-user visually 
 * Is there any way for a user to reactivate the activation lock after I've successfully disabled it?
   * If the device was manually enrolled AND the user has admin rights, activation lock would be reactivated once that MDM Profile is removed (either on the next reboot, or if the user toggles Find My off and on again).
   * Alternatively, if you have configured your MDM to `Allowed user-based Activation Lock', then activation lock will become active again once they turn Find My mac back on.
-* Why does System Settings open to the AppleID section rather than the iCloud section on macOS Ventura?
-  * As far as i'm aware Apple does not have a supported extension for that specific part of System Settings. If you know what it is, let me know! If that's a must have for your users then you can deploy a PPPC profile granting your MDM agent access to Accessibility and do some UI scripting with Applescript. I already have this lined up for version 2.
 * Why didn't you just use `nvram fmm-mobileme-token-FMM` to determine Activation Lock status?
   * That reports on whether FindMy is enabled, regardless of actual Activation Lock status.
   
 ## What's Next?
-* Not opening System Settings directly to the iCloud section of the Apple ID window in Ventura annoys me. I'll include some UI applescripting in v2 to handle that.
-
-* I'll also grab the e-mail address of the offending iCloud account and put that in the logs for reference.
-
-* Anything else you want to see in v.2? Weird Activation Lock edge cases? Let me know!
+* I've added everything I've wanted to see so far. Anything else you want to see in the next version? Weird Activation Lock edge cases? Let me know!
--- unActivationLock.zsh
+++ unActivationLock.zsh
@@ -10,8 +10,8 @@
 ########################################################################################
 # Created by Brian Van Peski - macOS Adventures
 ########################################################################################
-# Current version: 1.4 | See CHANGELOG for full version history.
-# Updated: 01/22/2023
+# Current version: 1.5 | See CHANGELOG for full version history.
+# Updated: 02/01/2023
 
 # Set logging - Send logs to stdout as well as Unified Log
 # Use 'log show --process "logger"'to view logs activity.
@@ -26,7 +26,20 @@ function LOGGING {
 # Messaging
 dialogTitle="Turn off Find My Mac"
 dialogMessage="This company device is currently locked to your iCloud account. Please turn off Find My Mac under iCloud > Find My Mac."
-appIcon="/System/Library/PrivateFrameworks/AOSUI.framework/Versions/A/Resources/iCloud.icns" #Path to app icon for messaging (optional)
+appIcon="/System/Library/PrivateFrameworks/AOSUI.framework/Versions/A/Resources/findmy.icns" #Path to app icon for messaging (optional)
+# SwiftDialog Options
+swiftDialogOptions=(
+  --mini
+  --ontop
+  --moveable
+)
+
+attempts=6 #How many attempts at prompting the user before giving up.
+wait_time=40 #How many seconds to wait between user prompts.
+
+# Change to 'true' if you want to *always* prompt the user to log out when Find My Mac
+# is enabled, regardless of user-based Activation Lock status.
+DisallowFindMy=false
 
 ##############################################################
 # VARIABLES & FUNCTIONS
@@ -37,6 +50,9 @@ plist="/Users/$currentUser/Library/Preferences/MobileMeAccounts.plist"
 DEPStatus=$(profiles status -type enrollment | grep "Enrolled via DEP" | awk '{print $4}')
 FindMyEnabled=$(/usr/libexec/PlistBuddy -c print "$plist" | grep -A1 "FIND_MY_MAC" | awk 'FNR == 2 {print $3}' ) #Checks dictionary to make sure proper user is targeted [if FindMy = then, continue]
 KandjiAgent="/Library/Kandji/Kandji Agent.app"
+#Path to SwiftDialog
+dialogPath="/usr/local/bin/dialog"
+dialogApp="/Library/Application Support/Dialog/Dialog.app"
 
 UserLookup (){
 ## Fetch all local user accounts, return account with iCloud FindMyStatus enabled.
@@ -51,6 +67,7 @@ for user in "${USER_LIST[@]}"; do
       if [[ $FindMyEnabled == "true" ]]; then
       LOGGING "Find My is enabled for user $user"
       FindMyUser="$user"
+      FindMyEmail=$(/usr/libexec/PlistBuddy -c 'print Accounts:0:AccountID' "/Users/$user/Library/Preferences/MobileMeAccounts.plist")
       fi
     else
       #LOGGING "No iCloud login found for $user"
@@ -59,14 +76,24 @@ done
 }
 
 UserDialog (){
-  # Check if $appIcon file exists on system, if not use standard dialog. If
-  # Kandji agent is not installed, default to applescript dialog.
-  if [[ -f $appIcon && ! -d $KandjiAgent ]]; then
+  #First check if the app icon exists
+  if [ -e "$appIcon" ]; then
+    iconCMD=(--icon "$appIcon")
+  else
+    #If the icon file doesn't exist, set an empty array to omit from dialogs.
+    iconCMD=()
+  fi
+
+  #If KandjiAgent is installed, use Kandji
+  if [[ -d "$KandjiAgent" ]]; then
+    /usr/local/bin/kandji display-alert --title "$dialogTitle" --message "$dialogMessage" ${iconCMD[@]}
+  #No Kandji, and SwiftDialog is installed, use SwiftDialog
+  elif [[ -e "$dialogPath" && -e "$dialogApp" ]]; then
+    "$dialogPath" --title "$dialogTitle" --message "$dialogMessage" ${swiftDialogOptions[@]} ${iconCMD[@]}
+  #No Kandji and no SwiftDialog, default to osascript w/ icon.
+  elif [ -e "$appIcon" ]; then
     /usr/bin/osascript -e 'display dialog "'$dialogMessage'" with title "'$dialogTitle'" with icon POSIX file "'$appIcon'" buttons {"Okay"} default button 1 giving up after 15'
-  elif [[ -f $appIcon && -d $KandjiAgent ]]; then
-    /usr/local/bin/kandji display-alert --title ''$dialogTitle'' --message ''$dialogMessage'' --icon $appIcon
-  elif [[ ! -f $appIcon && -d $KandjiAgent ]]; then
-    /usr/local/bin/kandji display-alert --title ''$dialogTitle'' --message ''$dialogMessage''
+  #No Kandji, no SwiftDialog, and no appicon. Use osascript.
   else
     /usr/bin/osascript -e 'display dialog "'$dialogMessage'" with title "'$dialogTitle'" buttons {"Okay"} default button 1 giving up after 15'
   fi
@@ -84,15 +111,21 @@ if pgrep "Liftoff" >/dev/null; then
 elif [[ $activationLock == "Enabled" ]]; then
   LOGGING "--- User-Based Activation Lock is: Enabled. Checking local users..."
   UserLookup
-  #echo "TEST - $plist $FindMyEnabled $FindMyUser $currentUser"
   #Determine the FindMy enabled user and see if it matches the currently logged in user.
   if [[ -f "$plist" && "$FindMyUser" == "$currentUser" ]]; then
+    dialogAttempts=0
     until [[ $activationLock == "Disabled" ]]
       do
+        if (( $dialogAttempts >= $attempts )); then
+          LOGGING "Prompts have been ignored after $attempts attempts. Giving up..."
+          exit 1
+        fi
         LOGGING "--- Found logged in iCloud account '$FindMyUser'... Presenting pane to user and requesting user to log out..."
-        open "x-apple.systempreferences:com.apple.preferences.AppleIDPrefPane"
+        open "x-apple.systempreferences:com.apple.preferences.AppleIDPrefPane?iCloud"
+        osascript -e 'tell application "System Settings"' -e 'activate' -e 'end tell'
         UserDialog
-        sleep 40
+        sleep $wait_time
+        ((dialogAttempts++))
         export activationLock=$(/usr/sbin/system_profiler SPHardwareDataType | awk '/Activation Lock Status/{print $NF}')
       done
     LOGGING "Activation Lock Status: $activationLock"
@@ -103,13 +136,34 @@ elif [[ $activationLock == "Enabled" ]]; then
       exit 1
     else
       LOGGING "--- The currently logged in user: '$currentUser' is not the user associated with the Activation Lock.
-      --- Activation Lock status is: $activationLock, and is locked by user '$FindMyUser'.
+      --- Activation Lock status is: $activationLock, and is locked by user '$FindMyUser' with account '$FindMyEmail'.
       --- Script will continue to run until the appropriate user logs in and is prompted to turn off Find My.
       Exiting..."
       exit 1
   fi
 else
+  UserLookup
+  # Always prompt if Find My Mac is enabled (optional)
+  if [[ $DisallowFindMy == true && "$FindMyUser" == "$currentUser" ]]; then
+    dialogAttempts=0
+    until [[ $FindMyEnabled == false ]]
+      do
+        if (( $dialogAttempts >= $attempts )); then
+        LOGGING "Prompts have been ignored after $attempts attempts. Giving up..."
+        exit 1
+        fi
+        LOGGING "--- Found logged in iCloud account for user '$FindMyUser' with account '$FindMyEmail'... Presenting pane to user and requesting user to log out of Find My Mac."
+        open "x-apple.systempreferences:com.apple.preferences.AppleIDPrefPane?iCloud"
+        osascript -e 'tell application "System Settings"' -e 'activate' -e 'end tell'
+        UserDialog
+        sleep $wait_time
+        ((dialogAttempts++))
+        #export FindMyStatus
+        export FindMyEnabled=$(/usr/libexec/PlistBuddy -c print "$plist" | grep -A1 "FIND_MY_MAC" | awk 'FNR == 2 {print $3}')
+      done
+  fi
   LOGGING "--- User-based Activation Lock not enabled.
+  --- Find My Mac status for $currentUser is: $FindMyEnabled.
   Exiting..."
- exit 0
+  exit 0
 fi
